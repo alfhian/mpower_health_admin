b270702fd1a38a9c8809b871746b4dc280b04df0
alfhiansuwandi <alfhian@drrewire.com>
2023-04-07 07:40:28 +0700

    update client_id field to id in client information table

--- app/Models/LabResult.php
+++ app/Models/LabResult.php
@@ -28,23 +28,11 @@ class LabResult extends Model
         'recommendation_file_path'  => 'encrypted',
     ];
 
-    public static function getLabResultRecommendation()
-    {
-        $data = labResult::select('lab_results.*', 'b.file_path as recommendation_file_path', 'b.recommendation', DB::raw('DATE_FORMAT(lab_results.created_at, "%b %e, %Y") as upload_date'))
-                ->leftJoin('recommendations as b', 'b.id', 'lab_results.recommendation_id')
-                ->where('client_id', Auth::id())
-                ->orderBy('created_at', 'desc')
-                ->orderBy('id', 'desc')
-                ->get();
-
-        return $data;
-    }
-
     public static function getLabResultAdmin()
     {
         $data = labResult::select('lab_results.*', 'b.file_path as recommendation_file_path', 'b.recommendation', 'b.user_input as admin', 'c.firstname', 'c.lastname', 'd.firstname as clerk_firstname', 'd.lastname as clerk_lastname', DB::raw('DATE_FORMAT(lab_results.created_at, "%b %e, %Y") as upload_date'))
                 ->leftJoin('recommendations as b', 'b.id', 'lab_results.recommendation_id')
-                ->leftJoin('client_information as c', 'c.client_id', 'lab_results.client_id')
+                ->leftJoin('client_information as c', 'c.id', 'lab_results.client_id')
                 ->leftJoin('user_information as d', 'd.id', 'lab_results.user_claim')
                 ->orderBy('created_at', 'desc')
                 ->orderBy('id', 'desc')
